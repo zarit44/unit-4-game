f7e7973d4ce8d41d464bffbbde3a54fb76f42bc9
zarito <zarit.ortiz@outlook.com>
2019-10-08 22:51:40 -0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # unit-4-game
+
+## Crystal collector game.
+
+The crystal collector game allows you to click on a crystal and each crystal has a number assigned to it. The computer will generate a random number. The goal is to match the number to the computers choice without going over. I made some improvements to this homework assignment. I made it so that the game restarts everytime you win and the crystals change numbers too with every game. 
+
--- game.js
+++ game.js
@@ -1,1 +1,92 @@
-alert("hi");
+  // var targetNum = Math.floor(Math.random()*20) + 100;
+  var targetNum = Math.floor(Math.random() * 20) + 100;
+  var counter = 0;
+  var numOptions = [1, 2, 3, 4];
+  var increment = numOptions[Math.round(Math.random() * 1) + 1];
+
+  function restart() {
+      targetNum = Math.floor(Math.random() * 20) + 100;
+      $("#baseNum").text(targetNum);
+      counter = 0;
+      $("#scoreNum").text(counter);
+  }
+
+
+
+
+
+  $("#baseNum").text(targetNum);
+  $("#scoreNum").text(counter);
+  
+
+  $("#btnOne").on("click", function () {
+
+      counter += 12;
+      $("#scoreNum").text(counter);
+
+
+      if (counter === targetNum) {
+
+          restart();
+      }
+
+      else if (counter >= targetNum) {
+
+          restart();
+      }
+
+
+  });
+
+  var counter = 0;
+  $("#btnTwo").on("click", function () {
+
+      counter += 8;
+      $("#scoreNum").text(counter);
+
+      if (counter === targetNum) {
+        
+          restart();
+      }
+
+      else if (counter >= targetNum) {
+          restart();
+      }
+
+  });
+
+  var counter = 0;
+  $("#btnThree").on("click", function () {
+
+      counter += 6;
+      $("#scoreNum").text(counter);
+
+
+      if (counter === targetNum) {
+          restart();
+      }
+
+      else if (counter >= targetNum) {
+          restart();
+      }
+
+  });
+
+  var counter = 0;
+  $("#btnFour").on("click", function () {
+
+      counter += 11;
+      $("#scoreNum").text(counter);
+
+
+      if (counter === targetNum) {
+          restart();
+      }
+
+      else if (counter >= targetNum) {
+          restart();
+      }
+
+  });
+
+
